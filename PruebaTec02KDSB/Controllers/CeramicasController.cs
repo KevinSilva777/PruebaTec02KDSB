1cc3ae623caf189dc278f5dba5e2b5c9629a0a72
KevinSilva777 <sb24002@esfe.agape.edu.sv>
2024-02-22 08:51:25 -0600

    Controlador y Vista de Editar modificadas para mostra imagen y la medidas

--- PruebaTec02KDSB/Controllers/CeramicasController.cs
+++ PruebaTec02KDSB/Controllers/CeramicasController.cs
@@ -84,7 +84,7 @@ namespace PruebaTec02KDSB.Controllers
             {
                 return NotFound();
             }
-            ViewData["TamañoId"] = new SelectList(_context.Medidas, "Id", "Id", ceramica.TamañoId);
+            ViewData["TamañoId"] = new SelectList(_context.Medidas, "Id", "Medida1", ceramica.TamañoId);
             return View(ceramica);
         }
 
@@ -93,78 +93,126 @@ namespace PruebaTec02KDSB.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Tipo,Precio,Color,Imagen,TamañoId")] Ceramica ceramica)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Tipo,Precio,Color,Imagen,TamañoId")] Ceramica ceramica, IFormFile imagen)
         {
             if (id != ceramica.Id)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            if (imagen != null && imagen.Length > 0)
             {
-                try
+                using (var memoryStream = new MemoryStream())
                 {
-                    _context.Update(ceramica);
-                    await _context.SaveChangesAsync();
+                    await imagen.CopyToAsync(memoryStream);
+                    ceramica.Imagen = memoryStream.ToArray();
                 }
-                catch (DbUpdateConcurrencyException)
+                _context.Update(ceramica);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                var CeramiFind = await _context.Ceramicas.FirstOrDefaultAsync(s => s.Id == ceramica.Id);
+                if (CeramiFind?.Imagen?.Length > 0)
+                    ceramica.Imagen = CeramiFind.Imagen;
+                CeramiFind.Nombre = ceramica.Nombre;
+                CeramiFind.Tipo = ceramica.Tipo;
+                CeramiFind.Precio = ceramica.Precio;
+                CeramiFind.Color = ceramica.Color;
+                CeramiFind.TamañoId = ceramica.TamañoId;
+                _context.Update(CeramiFind);
+                await _context.SaveChangesAsync();
+            }
+            try
+            {
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CeramicaExists(ceramica.Id))
                 {
-                    if (!CeramicaExists(ceramica.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    return NotFound();
                 }
-                return RedirectToAction(nameof(Index));
+                else
+                {
+                    throw;
+                } 
             }
-            ViewData["TamañoId"] = new SelectList(_context.Medidas, "Id", "Id", ceramica.TamañoId);
-            return View(ceramica);
-        }
-
-        // GET: Ceramicas/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Ceramicas == null)
-            {
-                return NotFound();
-            }
-
-            var ceramica = await _context.Ceramicas
-                .Include(c => c.Tamaño)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (ceramica == null)
-            {
-                return NotFound();
-            }
-
-            return View(ceramica);
-        }
-
-        // POST: Ceramicas/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Ceramicas == null)
-            {
-                return Problem("Entity set 'PruebaTec02KDSBDBContext.Ceramicas'  is null.");
-            }
-            var ceramica = await _context.Ceramicas.FindAsync(id);
-            if (ceramica != null)
-            {
-                _context.Ceramicas.Remove(ceramica);
-            }
-            
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+            
 
-        private bool CeramicaExists(int id)
-        {
-          return (_context.Ceramicas?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
+            //    if (id != ceramica.Id)
+            //    {
+            //        return NotFound();
+            //    }
+
+            //    if (ModelState.IsValid)
+            //    {
+            //        try
+            //        {
+            //            _context.Update(ceramica);
+            //            await _context.SaveChangesAsync();
+            //        }
+            //        catch (DbUpdateConcurrencyException)
+            //        {
+            //            if (!CeramicaExists(ceramica.Id))
+            //            {
+            //                return NotFound();
+            //            }
+            //            else
+            //            {
+            //                throw;
+            //            }
+            //        }
+            //        return RedirectToAction(nameof(Index));
+            //    }
+            //    ViewData["TamañoId"] = new SelectList(_context.Medidas, "Id", "Id", ceramica.TamañoId);
+            //    return View(ceramica);
+            //}
+
+            // GET: Ceramicas/Delete/5
+            public async Task<IActionResult> Delete(int? id)
+            {
+                if (id == null || _context.Ceramicas == null)
+                {
+                    return NotFound();
+                }
+
+                var ceramica = await _context.Ceramicas
+                    .Include(c => c.Tamaño)
+                    .FirstOrDefaultAsync(m => m.Id == id);
+                if (ceramica == null)
+                {
+                    return NotFound();
+                }
+
+                return View(ceramica);
+            }
+
+            // POST: Ceramicas/Delete/5
+            [HttpPost, ActionName("Delete")]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> DeleteConfirmed(int id)
+            {
+                if (_context.Ceramicas == null)
+                {
+                    return Problem("Entity set 'PruebaTec02KDSBDBContext.Ceramicas'  is null.");
+                }
+                var ceramica = await _context.Ceramicas.FindAsync(id);
+                if (ceramica != null)
+                {
+                    _context.Ceramicas.Remove(ceramica);
+                }
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            private bool CeramicaExists(int id)
+            {
+                return (_context.Ceramicas?.Any(e => e.Id == id)).GetValueOrDefault();
+            }
+        
     }
 }
